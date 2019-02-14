24e2205571fc0b6688aa3529ace752d5378e1afc
shoxruh <qutlimurodov95@bk.ru>
2019-02-14 18:59:05 +0300

    2_topshiriq

---
+++ index1.php
@@ -0,0 +1,67 @@
+ <html>
+ <head>
+ <title>brinchi topshriq
+ </title>
+ <body bgcolor="#5d5051" text="#ffffff">
+<form action"GET">
+
+<table style="width 50px; marjin 10px">
+ <tr>
+ <td>
+ ism;<br>
+<input type="text" name="ism">
+ <?php
+ echo $ism ='<b>'.$_GET['ism'];
+ ?>
+ </td>
+  </tr>
+  </table>
+  <table>
+ <tr>
+ <td>
+ familya;<br>
+           <input type="text" name="familya">
+ <?php
+ echo $familya ='<b>'.$_GET['familya'];
+ ?>
+ </td>
+  </tr>
+  <table>
+ <tr>
+ <td>
+ tug`ilgan_yil;<br>
+<input type="text" name="tug`ilgan_yil">
+ <?php
+ echo $tugilgan_yil ='<b>'.$_GET['tug`ilgan_yil'];
+ ?>
+ </td>
+  </tr>
+ <table>
+ <tr>
+ <td>
+ milati;<br>
+<input type="text" name="milati">
+
+ <?php
+ echo $milati ='<b>'.$_GET['milati'];
+ ?>
+ </td>
+  </tr>
+ </table>
+ <table>
+ <tr>
+ <td>
+ guruh;<br>
+<input type="text" name="guruh">
+
+ <?php
+ echo $guruh ='<b>'.$_GET['guruh'];
+ ?>
+ </td>
+ </table>
+<button  type="submit": align="center" >Yuborish</button>
+ </form>
+            
+ </body>
+ </head>
+ </html>
---
+++ index2.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>topshiriq 2</title>
+<body>
+<?php
+$massiv=array(24,qutlimurodov shoxruh,programist)
+if($massiv[0]<=40 && $massiv[3]==ture){
+	echo yaroqli;
+}
+	else{
+		echo yaroqsiz;
+	}
+	?>
+	</body>
+	</head>
+</html>
